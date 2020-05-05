3abfb321e267a412a883da9a57229d2cfd89df48
xiao.xinmiao <xiaoxm_001@163.com>
2020-05-05 16:43:09 +0800

    admin 管理工具

--- testModel/admin.py
+++ testModel/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from testModel.models import Test,Contact,Tag
+
+class TagInline(admin.TabularInline):
+    model =Tag
 
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name','age', 'email') 
+    search_fields = ('name',)
+    inlines =[TagInline]
+    # fields = ("name","email")
+    fieldsets =(
+        ["Main",{
+            "fields":("name","email"),
+        }],
+        ["Advance",{
+            "classes":("collapse",),
+            "fields":("age",),
+        }],
+    )
+
+
+admin.site.register(Contact,ContactAdmin)
+admin.site.register([Test,Tag])
--- testModel/models.py
+++ testModel/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class Test(models.Model):
     name = models.CharField(max_length =20)
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.IntegerField(default=0)
+    email = models.EmailField()
+    def __unicode__(self):
+        return self.name
+    
+class Tag(models.Model):
+    # DO_NOTHING CASCADE
+    contact = models.ForeignKey(Contact,on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=50)
+    def __unicode__(self):
+        return self.name
